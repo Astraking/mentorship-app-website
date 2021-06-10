3354b133b154d7a0c92fce9906c2a3f4dcaa3ab3
Addo Bernard Tawiah <65928766+BernardAddo@users.noreply.github.com>
2021-06-10 16:15:15 +0000

    Add ben as a contributor

--- src/components/contributors.tsx
+++ src/components/contributors.tsx
@@ -27,6 +27,10 @@ const contributors: GitHubProfileProps[] = [
         userName: 'yeboah326',
         role: 'Back-End Engineer',
     },
+    {
+        userName: 'BernardAddo',
+        role: 'Back-End Engineer',
+    },
 ];
 
 export const GitHubProfile = (props: GitHubProfileProps) => {
